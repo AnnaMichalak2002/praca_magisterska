d7a340c5e641f0fd2d03ec19573be76e79eb5c69
Anna Michalak <annamichalak.prywatne@gmail.com>
2026-07-04 11:36:38 +0200

    Run benchmark for grammar mode and run validation for each mode

--- run_benchmark.py
+++ run_benchmark.py
@@ -986,10 +986,8 @@ def run_vocabulary():
 def main():
     #run_writing()
     #run_vocabulary()
-    #run_grammar()
-    run_test_cefr()
-    #run_grammar()
-    #run_vocabulary()
+    #run_test_cefr()
+    run_grammar()
 
 
 if __name__ == "__main__":
